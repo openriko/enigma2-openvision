7009b09cdae52de1d0ebd4ffd00656d33f0fec64
openriko <104708508+openriko@users.noreply.github.com>
2025-10-18 21:42:01 +0100

    Update servicedvd.cpp

--- lib/service/servicedvd.cpp
+++ lib/service/servicedvd.cpp
@@ -1172,7 +1172,7 @@ void eServiceDVD::saveCuesheet()
 
 eAutoInitPtr<eServiceFactoryDVD> init_eServiceFactoryDVD(eAutoInitNumbers::service+1, "eServiceFactoryDVD");
 
-#if PY_MAJOR_VERSION >= 3
+
 	static struct PyModuleDef servicedvd_moduledef = {
 		PyModuleDef_HEAD_INIT,
 		"servicedvd",		/* m_name */
@@ -1184,14 +1184,12 @@ eAutoInitPtr<eServiceFactoryDVD> init_eServiceFactoryDVD(eAutoInitNumbers::servi
 		NULL,			/* m_clear */
 		NULL,			/* m_free */
 	};
-#endif
+
 
 PyMODINIT_FUNC
 initservicedvd(void)
 {
-#if PY_MAJOR_VERSION >= 3
+
 	PyModule_Create(&servicedvd_moduledef);
-#else
-	Py_InitModule("servicedvd", NULL);
-#endif
+
 }
