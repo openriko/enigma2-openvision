2dd6d1654bd964b24ffbdb40c63069dd9056c6b6
openriko <104708508+openriko@users.noreply.github.com>
2025-10-20 04:43:10 +0100

    Update core.cpp

--- lib/nav/core.cpp
+++ lib/nav/core.cpp
@@ -47,6 +47,11 @@ RESULT eNavigation::playService(const eServiceReference &service)
 	m_runningServiceRef = service;
 	return res;
 }
+RESULT eNavigation::setPiPService(const eServiceReference &service)
+{
+	m_runningPiPServiceRef = service;
+	return 0;
+}
 
 
 RESULT eNavigation::connectEvent(const sigc::slot<void(int)> &event, ePtr<eConnection> &connection)
