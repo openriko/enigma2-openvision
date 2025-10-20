229eb9fae9bdea4595399763c7b1ab80a79b5347
openriko <104708508+openriko@users.noreply.github.com>
2025-10-20 13:08:51 +0100

    Update core.h

--- lib/nav/core.h
+++ lib/nav/core.h
@@ -39,7 +39,7 @@ public:
 	RESULT clearPiPService();
     RESULT pause(int p);
     SWIG_VOID(RESULT) getCurrentService(ePtr<iPlayableService> &SWIG_OUTPUT);
-
+};
 
 class eNavigation: public iObject, public sigc::trackable
 {
